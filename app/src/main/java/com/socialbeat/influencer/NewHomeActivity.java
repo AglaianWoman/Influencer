787c6f16f9edfc564a10ea32ef61944d22fca049
karthikdhanajeyan <karthikdhanajeyan@gmail.com>
2018-02-06 14:00:14 +0530

    First live check Changes

--- app/src/main/java/com/socialbeat/influencer/NewHomeActivity.java
+++ app/src/main/java/com/socialbeat/influencer/NewHomeActivity.java
@@ -131,7 +131,7 @@ public class NewHomeActivity extends AppCompatActivity implements NavigationView
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
-
+//navigation
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
